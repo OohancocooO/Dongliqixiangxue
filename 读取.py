40f7e7e1d9bffb17f2b0e6d89fb7ce2bc245886c
OpenYNU <hanson4201@outlook.com>
2024-09-24 14:35:27 +0800

    更新 读取.py

--- 读取.py
+++ 读取.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import xarray as xr
 
 
-dataset = xr.open_dataset("xiaochidu.nc")
+dataset = xr.open_dataset("single levels.nc")
 
-# print(dataset)
+print(dataset)
 # print(dataset.data_vars)
