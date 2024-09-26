55ae3d3a0ffcf37db0da6ab47a7cc17f2e1bf3b2
OpenYNU <hanson4201@outlook.com>
2024-09-26 17:22:24 +0800

    更新 读取.py

---
+++ 读取.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+
+
+dataset = xr.open_dataset("ERA5 hourly data on pressure levels from 1940 to present.nc")
+
+print(dataset)
+# print(dataset.data_vars)
