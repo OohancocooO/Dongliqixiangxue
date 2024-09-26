5fdb83f65df82bafbc1de433a37352b1b7004f6c
OpenYNU <hanson4201@outlook.com>
2024-09-26 16:52:19 +0800

    1

--- 读取.py
+++ 读取.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import xarray as xr
 
 
-dataset = xr.open_dataset("single levels.nc")
+dataset = xr.open_dataset("ERA5 hourly data on pressure levels from 1940 to present.nc")
 
 print(dataset)
 # print(dataset.data_vars)
