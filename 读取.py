5a99b63c3887d1e8bb3b9dc800771d7b8ee01e38
OpenYNU <hanson4201@outlook.com>
2024-10-10 19:02:55 +0800

    Update 读取.py

--- 读取.py
+++ 读取.py
@@ -6,8 +6,8 @@ import xarray as xr
 dataset = xr.open_dataset("xiaochidu.nc")
 dataset2 = xr.open_dataset(r"D:\Study\Yunnan Uni\pressure levels china.nc")
 dataset3 = xr.open_dataset(r"D:\Study\Yunnan Uni\data_stream-oper.nc")
-# dataset3 = xr.open_dataset(r"D:\Study\Yunnan Uni\data_stream-wave.nc")
-dataset4 = xr.open_dataset("wind.nc")
+dataset3 = xr.open_dataset(r"D:\Study\Yunnan Uni\data_stream-wave.nc")
+dataset4 = xr.open_dataset(r"D:\Download\presure level.nc")
 
 # print(dataset)
 print(dataset4)
